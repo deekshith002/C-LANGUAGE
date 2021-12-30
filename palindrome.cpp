4a7b406dbcdc2571afc786625d8b4bce566fc305
deekshith002 <96228591+deekshith002@users.noreply.github.com>
2021-12-30 14:31:47 +0530

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+	int n,k,i=0,m,sum=0;
+	printf("enter a number:");
+	scanf("%d",&n);
+	m = n;
+	while(n>0)
+	{
+		k = n%10;
+		
+	}
+	printf("%d \n",sum);
+    if(m==sum)
+    printf("it is palindrome");
+    else
+    printf("it is not a palindrome");
+
+}
